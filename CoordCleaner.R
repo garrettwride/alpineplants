27ccfc4829fe40576baede7c97601caf66073f79
adellep <adellebelen@gmail.com>
2026-02-05 13:45:34 -0700

    occ_search trial

--- CoordCleaner.R
+++ CoordCleaner.R
@@ -1,2 +1,8 @@
 install.packages("CoordinateCleaner")
+install.packages("rgbif")
+install.packages("sf")
+
 library(CoordinateCleaner)
+library(rgbif)
+
+occ_search(taxonKey=5384754, country="US;CA", basisOfRecord="HUMAN_OBSERVATION")

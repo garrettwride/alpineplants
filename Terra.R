ad4b1c1db4a5d2d3d3a45589fc68a11ba4623091
kristinbillings <kris10d@byu.edu>
2026-02-05 13:42:59 -0700

    fix rockmt

--- Terra.R
+++ Terra.R
@@ -1,5 +1,6 @@
 library(terra)
 library(geodata)
+library(sf)
 
 
 # How I got the region in the first place (download lable areas from here: https://www.naturalearthdata.com/downloads/50m-physical-vectors/50m-physical-labels/)
@@ -12,14 +13,14 @@ library(geodata)
 #st_write(rockies, "~/Desktop/rocky_mountains.shp")
 
 # This is a map of the US and Canada rectangle. (need gadm files to run, not on git bc big)
-#us <- gadm(country="USA", level=0, path=".")
-#can <- gadm(country="CAN", level=0, path=".")
-#mapData <- rbind(us, can)
-#myBox <- ext(-128, -103, 35, 60)
-#regionalMap <- crop(mapData, myBox)
+# us <- gadm(country="USA", level=0, path=".")
+# can <- gadm(country="CAN", level=0, path=".")
+# mapData <- rbind(us, can)
+# myBox <- ext(-128, -103, 35, 60)
+# regionalMap <- crop(mapData, myBox)
 
 rockyMntPoly <-st_read("./RockyMountainsRegion/rocky_mountains.shp")
 rockyMntSpat <- vect(rockyMntPoly)
 
 #plot(regionalMap, col="lightblue", main="US and Canada Map")
-plot(rockyMntPoly, col="lightgreen") # add this argument if printing on map col="lightblue",
+plot(rockyMntPoly, col="lightgreen") # add this argument if printing on map , add=TRUE,

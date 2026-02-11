86add34f64f73ecd60ca5a9c99346b2d59532c9a
Garrett Johnson <garretttolmanj@gmail.com>
2026-02-10 19:46:11 -0700

    Wrote pseudocode for biomod pipeline. Added prepareOccurrenceData.R which can be used for getting the gbif data into the correct format for biomod.

--- biomodPipeline.R
+++ biomodPipeline.R
@@ -1,6 +1,71 @@
 library(biomod2)
 library(terra)
 
+algorithms   <- c("GLM", "GAM", "RF") # "MAXENT"
+pa_strategies <- c("random", "disk")
+pa_numbers    <- c(1, 5, 10)   # × presences
+n_boot        <- 10
+
+# outermost loop
+# for (species in species_list) 
+#     myRespName <- species$name
+      # myResp <- species$response
+      # myRespXY <- species$coordinates
+      # SpeciesType <- species$type # Generalist/Specialist
+
+# Bootsrap occurance data (next loop)
+# for (b in range(n_boot))
+  # set.seed(b)
+  # pres_idx <- which(myResp == 1)
+  # boot_pres <- sample(pres_idx, replace = TRUE)
+  # 
+  # boot_resp <- rep(0, length(myResp))
+  # boot_resp[boot_pres] <- 1
+
+# Set up Pseudo-absences (next 2 loops)
+for (pa_strat in pa_strategies) {
+  for (pa_mult in pa_numbers) {
+    PA_n <- pa_mult * length(pres_idx)
+    
+    myBiomodData <- BIOMOD_FormatingData(
+      resp.var = boot_resp,
+      expl.var = myExpl,
+      resp.xy  = myRespXY,
+      resp.name = paste(
+        myRespName, "boot", b, pa_strat, pa_mult, sep = "_"),
+      PA.nb.rep = 1,
+      PA.nb.absences = PA_n,
+      PA.strategy = pa_strat,
+      PA.dist.min = 10,
+      PA.dist.max = 20
+    )
+    
+    myBiomodModelOut <- BIOMOD_Modeling(
+      bm.format = myBiomodData,
+      models = algorithms,
+      CV.strategy = "random",
+      CV.nb.rep = 1,
+      CV.perc = 0.8,
+      CV.do.full.models = TRUE,
+      metric.eval = c("TSS", "AUC")
+    )
+    ## Save the rasters/analyze them
+    myBiomodProj <- BIOMOD_Projection(
+      bm.mod = myBiomodModelOut,
+      proj.name = paste0("Current_boot", b),
+      new.env = myExpl,
+      models.chosen = "all",
+      metric.binary = "TSS",
+      metric.filter = "all",
+      build.clamping.mask = TRUE
+    )
+  }  
+}
+
+#     BIOMOD_FormatingData (PA.nb.rep = 1)
+#     BIOMOD_Modeling (CV optional)
+#     BIOMOD_Projection
+
 # Load species occurrences (6 species available)
 data('DataSpecies')
 head(DataSpecies)
@@ -19,70 +84,33 @@ data('bioclim_current')
 myExpl <- rast(bioclim_current)
 print(myExpl)
 
-# Format data with true absences
-myBiomodData <- BIOMOD_FormatingData(dir.name = 'testFiles/',
-                                     resp.name = myRespName,
-                                     resp.var = myResp,
-                                     resp.xy = myRespXY,
-                                     expl.var = myExpl)
-myBiomodData
-plot(myBiomodData)
+# # Transform true absences into potential pseudo-absences
+myResp.PA <- ifelse(myResp == 1, 1, NA)
 
-# # # Transform true absences into potential pseudo-absences
-# myResp.PA <- ifelse(myResp == 1, 1, NA)
-# 
-# # # Format data with pseudo-absences : random method
-# myBiomodData.r <- BIOMOD_FormatingData(resp.var = myResp.PA,
-#                                        expl.var = myExpl,
-#                                        resp.xy = myRespXY,
-#                                        resp.name = myRespName,
-#                                        PA.nb.rep = 4,
-#                                        PA.nb.absences = 1000,
-#                                        PA.strategy = 'random')
-# 
-# myBiomodData.r
-# plot(myBiomodData.r)
-# 
-# # # Select multiple sets of pseudo-absences
-# # # Transform true absences into potential pseudo-absences
-# myResp.PA <- ifelse(myResp == 1, 1, NA)
-# #
-# # # Format Data with pseudo-absences : random method
-# myBiomodData.multi <- BIOMOD_FormatingData(resp.var = myResp.PA,
-#                                            expl.var = myExpl,
-#                                            resp.xy = myRespXY,
-#                                            resp.name = myRespName,
-#                                            PA.nb.rep = 4,
-#                                            PA.nb.absences = c(1000, 500, 500, 200),
-#                                            PA.strategy = 'random')
-# myBiomodData.multi
-# summary(myBiomodData.multi)
-# plot(myBiomodData.multi)
+# # Format data with pseudo-absences : random method
+myBiomodData.r <- BIOMOD_FormatingData(dir.name = 'testFiles/',
+                                       resp.var = myResp.PA,
+                                       expl.var = myExpl,
+                                       resp.xy = myRespXY,
+                                       resp.name = myRespName,
+                                       PA.nb.rep = 1,
+                                       PA.nb.absences = 100,
+                                       PA.strategy = 'disk',
+                                       PA.dist.min = 10,
+                                       PA.dist.max = 20)
 
-# # # k-fold selection
-# cv.k <- bm_CrossValidation(bm.format = myBiomodData,
-#                            strategy = 'kfold',
-#                            nb.rep = 2,
-#                            k = 3)
-# 
-# # stratified selection (geographic)
-# cv.s <- bm_CrossValidation(bm.format = myBiomodData,
-#                            strategy = 'strat',
-#                            k = 2,
-#                            balance = 'presences',
-#                            strat = 'x')
-
+myBiomodData.r
+plot(myBiomodData.r)
 
 
 # Model single models
-myBiomodModelOut <- BIOMOD_Modeling(bm.format = myBiomodData,
+myBiomodModelOut <- BIOMOD_Modeling(bm.format = myBiomodData.r,
                                     models = c('GLM'),
                                     CV.strategy = 'random',
-                                    CV.nb.rep = 2,
                                     CV.perc = 0.8,
-                                    OPT.strategy = 'bigboss',
-                                    metric.eval = 'TSS',
-                                    var.import = 3)
+                                    CV.do.full.models = FALSE,
+                                    OPT.strategy = 'default',
+                                    metric.eval = 'TSS')
 # seed.val = 123)
 # nb.cpu = 8)
 myBiomodModelOut
@@ -90,71 +118,6 @@ myBiomodModelOut
 # Get evaluation scores & variables importance
 get_evaluations(myBiomodModelOut)
 get_variables_importance(myBiomodModelOut)
-
-# Represent evaluation scores & variables importance
-bm_PlotEvalMean(bm.out = myBiomodModelOut, dataset = 'calibration')
-bm_PlotEvalMean(bm.out = myBiomodModelOut, dataset = 'validation')
-bm_PlotEvalBoxplot(bm.out = myBiomodModelOut, dataset = 'calibration', group.by = c('algo', 'algo'))
-bm_PlotEvalBoxplot(bm.out = myBiomodModelOut, dataset = 'calibration', group.by = c('algo', 'run'))
-bm_PlotVarImpBoxplot(bm.out = myBiomodModelOut, group.by = c('expl.var', 'algo', 'algo'))
-bm_PlotVarImpBoxplot(bm.out = myBiomodModelOut, group.by = c('expl.var', 'algo', 'run'))
-bm_PlotVarImpBoxplot(bm.out = myBiomodModelOut, group.by = c('algo', 'expl.var', 'run'))
-
-# Represent response curves
-bm_PlotResponseCurves(bm.out = myBiomodModelOut, 
-                      models.chosen = get_built_models(myBiomodModelOut)[c(1, 4, 8, 10, 13)],
-                      fixed.var = 'median')
-bm_PlotResponseCurves(bm.out = myBiomodModelOut, 
-                      models.chosen = get_built_models(myBiomodModelOut)[c(1, 4, 8, 10, 13)],
-                      fixed.var = 'min')
-bm_PlotResponseCurves(bm.out = myBiomodModelOut, 
-                      models.chosen = get_built_models(myBiomodModelOut)[4],
-                      fixed.var = 'median',
-                      do.bivariate = TRUE)
-
-# Explore models' outliers & residuals
-bm_ModelAnalysis(bm.mod = myBiomodModelOut,
-                 models.chosen = get_built_models(myBiomodModelOut)[c(1, 4, 8, 10, 13)])
-
-
-
-# Model ensemble models
-myBiomodEM <- BIOMOD_EnsembleModeling(bm.mod = myBiomodModelOut,
-                                      models.chosen = 'all',
-                                      em.by = 'all',
-                                      em.algo = c('EMmedian', 'EMmean', 'EMwmean',
-                                                  'EMca', 'EMci', 'EMcv'),
-                                      metric.select = c('TSS'),
-                                      metric.select.thresh = c(0.7),
-                                      metric.eval = c('TSS', 'AUCroc', 'BOYCE'),
-                                      var.import = 3,
-                                      EMci.alpha = 0.05,
-                                      EMwmean.decay = 'proportional')
-myBiomodEM
-
-# Get evaluation scores & variables importance
-get_evaluations(myBiomodEM)
-get_variables_importance(myBiomodEM)
-
-# Represent evaluation scores & variables importance
-bm_PlotEvalMean(bm.out = myBiomodEM, dataset = 'calibration', group.by = 'full.name')
-bm_PlotEvalBoxplot(bm.out = myBiomodEM, dataset = 'calibration', group.by = c('full.name', 'full.name'))
-bm_PlotVarImpBoxplot(bm.out = myBiomodEM, group.by = c('expl.var', 'full.name', 'full.name'))
-bm_PlotVarImpBoxplot(bm.out = myBiomodEM, group.by = c('expl.var', 'algo', 'merged.by.run'))
-bm_PlotVarImpBoxplot(bm.out = myBiomodEM, group.by = c('algo', 'expl.var', 'merged.by.run'))
-
-# Represent response curves
-bm_PlotResponseCurves(bm.out = myBiomodEM, 
-                      models.chosen = get_built_models(myBiomodEM)[c(1, 6, 7)],
-                      fixed.var = 'median')
-bm_PlotResponseCurves(bm.out = myBiomodEM, 
-                      models.chosen = get_built_models(myBiomodEM)[c(1, 6, 7)],
-                      fixed.var = 'min')
-bm_PlotResponseCurves(bm.out = myBiomodEM, 
-                      models.chosen = get_built_models(myBiomodEM)[7],
-                      fixed.var = 'median',
-                      do.bivariate = TRUE)
-
 
 # Project single models
 myBiomodProj <- BIOMOD_Projection(bm.mod = myBiomodModelOut,
@@ -166,3 +129,42 @@ myBiomodProj <- BIOMOD_Projection(bm.mod = myBiomodModelOut,
                                   build.clamping.mask = TRUE)
 myBiomodProj
 plot(myBiomodProj)
+get_predictions(myBiomodProj)
+
+# 
+# # Model ensemble models
+# myBiomodEM <- BIOMOD_EnsembleModeling(bm.mod = myBiomodModelOut,
+#                                       models.chosen = 'all',
+#                                       em.by = 'all',
+#                                       em.algo = c('EMmedian', 'EMmean', 'EMwmean',
+#                                                   'EMca', 'EMci', 'EMcv'),
+#                                       metric.select = c('TSS'),
+#                                       metric.select.thresh = c(0.7),
+#                                       metric.eval = c('TSS', 'AUCroc', 'BOYCE'),
+#                                       var.import = 3,
+#                                       EMci.alpha = 0.05,
+#                                       EMwmean.decay = 'proportional')
+# myBiomodEM
+# 
+# # Get evaluation scores & variables importance
+# get_evaluations(myBiomodEM)
+# get_variables_importance(myBiomodEM)
+# 
+# # Represent evaluation scores & variables importance
+# bm_PlotEvalMean(bm.out = myBiomodEM, dataset = 'calibration', group.by = 'full.name')
+# bm_PlotEvalBoxplot(bm.out = myBiomodEM, dataset = 'calibration', group.by = c('full.name', 'full.name'))
+# bm_PlotVarImpBoxplot(bm.out = myBiomodEM, group.by = c('expl.var', 'full.name', 'full.name'))
+# bm_PlotVarImpBoxplot(bm.out = myBiomodEM, group.by = c('expl.var', 'algo', 'merged.by.run'))
+# bm_PlotVarImpBoxplot(bm.out = myBiomodEM, group.by = c('algo', 'expl.var', 'merged.by.run'))
+# 
+# # Represent response curves
+# bm_PlotResponseCurves(bm.out = myBiomodEM, 
+#                       models.chosen = get_built_models(myBiomodEM)[c(1, 6, 7)],
+#                       fixed.var = 'median')
+# bm_PlotResponseCurves(bm.out = myBiomodEM, 
+#                       models.chosen = get_built_models(myBiomodEM)[c(1, 6, 7)],
+#                       fixed.var = 'min')
+# bm_PlotResponseCurves(bm.out = myBiomodEM, 
+#                       models.chosen = get_built_models(myBiomodEM)[7],
+#                       fixed.var = 'median',
+#                       do.bivariate = TRUE)
---
+++ prepareOccurrenceData.R
@@ -0,0 +1,44 @@
+library(rgbif)
+library(sf)
+library(dplyr)
+
+hackberry <- occ_search(
+  scientificName = "Celtis reticulata",
+  hasCoordinate = TRUE,
+  limit = 100000
+)
+
+# Extract the data frame
+hack_data <- hackberry$data
+
+  )
+
+occ_sf <- hack_data(
+  occ_clean,
+  coords = c("decimalLongitude", "decimalLatitude"),
+  crs = 4326
+)
+
+rocky_poly <- st_read("./RockyMountainsRegion/rocky_mountains.shp")
+rocky_poly <- st_transform(rocky_poly, 4326)  # Make sure CRS matches occurrences
+
+occ_rocky <- st_intersection(occ_sf, rocky_poly)
+occ_rocky <- occ_sf[st_within(occ_sf, rocky_poly, sparse = FALSE), ]
+
+# occ_rocky <- occ_sf[st_within(occ_sf, rocky_poly, sparse = FALSE), ]
+occ_rocky_df <- cbind(
+  st_drop_geometry(occ_rocky),
+  st_coordinates(occ_rocky)
+)
+
+# Rename coordinate columns correctly
+colnames(occ_rocky_df)[ncol(occ_rocky_df)-1:0] <- c("decimalLongitude", "decimalLatitude")
+# Presence points
+resp.var <- rep(1, nrow(occ_rocky_df))
+
+# Coordinates
+resp.xy <- occ_rocky_df[, c("decimalLongitude", "decimalLatitude")]
+
+# Name for BIOMOD
+resp.name <- "Celtis_reticulata"
+

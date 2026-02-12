a3953bfef3699a41970f999a785589681762facf
Garrett Johnson <garretttolmanj@gmail.com>
2026-02-12 13:13:53 -0700

    Added worldClim.R

---
+++ worldClim.R
@@ -0,0 +1,21 @@
+## Get Environmental Data
+library(geodata)
+library(terra)
+library(rgbif)
+library(sf)
+library(dplyr)
+
+rocky_poly <- st_read("./RockyMountainsRegion/rocky_mountains.shp")
+rocky_poly <- st_transform(rocky_poly, 4326)
+
+bio <- worldclim_global(var = "bio", res = 10, path = "./")
+rocky_vect <- vect(rocky_poly)
+
+bio_rocky <- crop(bio, rocky_vect)
+bio_rocky <- mask(bio_rocky, rocky_vect)
+bio_rocky
+
+myExpl <- bio_rocky[[c(1, 5, 12)]]
+
+plot(myExpl[[1]])
+points(resp.xy, col="red", pch=16)

f04145ebc61bdb17addc4db6fa1e95289a255053
kristinbillings <kris10d@byu.edu>
2026-03-10 12:57:35 -0600

    updated lmm

--- LMM.R
+++ LMM.R
@@ -5,7 +5,8 @@ library(ggplot2)
 library(dplyr)
 library(gridExtra)
 
-biomod_data <- read.csv("./testFiles/testLme4Data.csv")
+biomod_data <- read.csv("./testFiles/SDM_results_for_LMM.csv")
+
 
 # Normalize MeanSuitability, SuitableArea, SchoenersD, ElevationCentroid
 # (Generalists and Specialists combined)
@@ -27,41 +28,42 @@ biomod_data$normalizedElevationCentroid <- scale(biomod_data$ElevationCentroid)
 
 # for SPECIALISTS VS GENERALISTS as a whole: 
 # Random Effects = Species, Bootstrap
-# Fixed Effects = SpeciesType, Algorithm, PAStrategy, PANumber
+# Fixed Effects = NicheBreadth, Algorithm, PAStrategy, PANumber
 
 # Response Variable = Schoener's D
 schoener_resp_var <- lmer(normalizedSchoenersD ~ 
-        SpeciesType + Algorithm + PAStrategy + PANumber +
-        SpeciesType:Algorithm + 
+        NicheBreadth + Algorithm + PAStrategy + PANumber +
+        NicheBreadth:Algorithm + 
         (1 | Species) + (1 | Species:Bootstrap),
         data = biomod_data,
         REML = FALSE )
+#schoener_resp_var
 
 # Response Variable = Mean Suitability
 meansuit_resp_var <- lmer(normalizedMeanSuitability ~ 
-        SpeciesType + Algorithm + PAStrategy + PANumber +
-        SpeciesType:Algorithm + 
+        NicheBreadth + Algorithm + PAStrategy + PANumber +
+        NicheBreadth:Algorithm + 
         (1 | Species) + (1 | Species:Bootstrap),
         data = biomod_data,
         REML = FALSE )
 
 # Response Variable = Suitable Area
 suitarea_resp_var <- lmer(normalizedSuitableArea ~ 
-        SpeciesType + Algorithm  + PAStrategy + PANumber +
-        SpeciesType:Algorithm + 
+        NicheBreadth + Algorithm  + PAStrategy + PANumber +
+        NicheBreadth:Algorithm + 
         (1 | Species) + (1 | Species:Bootstrap),
         data = biomod_data,
         REML = FALSE )
 
 # Response Variable =  Elevation Centroid
 elevcentroid_resp_var <- lmer(normalizedElevationCentroid ~ 
-        SpeciesType + Algorithm + PAStrategy + PANumber +
-        SpeciesType:Algorithm + 
+        NicheBreadth + Algorithm + PAStrategy + PANumber +
+        NicheBreadth:Algorithm + 
         (1 | Species) + (1 | Species:Bootstrap),
         data = biomod_data,
         REML = FALSE )
 
-# summary(schoener_resp_var)
+summary(schoener_resp_var)
 # summary(meansuit_resp_var)
 # summary(suitarea_resp_var)
 # summary(elevcentroid_resp_var)
@@ -88,6 +90,7 @@ elevcentroid_var_r2 = r2beta(elevcentroid_resp_var,
 
 # plotting variances 
 schoener_plot <- plot(schoener_var_r2) + ggtitle("Marginal Schoener R²")
+schoener_plot
 meansuit_plot <- plot(meansuit_var_r2) + ggtitle("Marginal Mean Predicted Suitability R²")
 suitarea_plot <- plot(suitarea_var_r2) + ggtitle("Marginal Suitable Area R²")
 elevcentroid_plot <- plot(elevcentroid_var_r2) + ggtitle("Marginal Elevartion Centroid R²")
@@ -96,30 +99,53 @@ grid.arrange(schoener_plot, meansuit_plot, suitarea_plot, elevcentroid_plot, nco
 
 # CONDITIONAL calculation of PERCENT VARIANCE (fixed AND random variables)
 
-# Predicted Response variable based on 2nd argument (so SpeciesType) 
-emm_schoener = emmeans(schoener_resp_var, ~SpeciesType)
-emm_meansuit = emmeans(meansuit_resp_var, ~SpeciesType)
-emm_suitarea = emmeans(suitarea_resp_var, ~SpeciesType)
-emm_elevcentroid = emmeans(elevcentroid_resp_var, ~SpeciesType)
+# How each ResponceVariable changes when NicheBreadth changes, within each Bootstrap
+emt_algorithm_schoener = emtrends(schoener_resp_var, ~Bootstrap, var = "NicheBreadth")
+emt_algorithm_meansuit = emtrends(meansuit_resp_var, ~Bootstrap, var = "NicheBreadth")
+emt_algorithm_suitarea = emtrends(suitarea_resp_var, ~Bootstrap, var = "NicheBreadth")
+emt_algorithm_elevcentroid = emtrends(elevcentroid_resp_var, ~Bootstrap, var = "NicheBreadth")
 
-schoener_df = as.data.frame(emm_schoener) %>%
+# How each ResponceVariable changes when NicheBreadth changes, within each Algorithm
+emt_algorithm_schoener = emtrends(schoener_resp_var, ~Algorithm, var = "NicheBreadth")
+emt_algorithm_meansuit = emtrends(meansuit_resp_var, ~Algorithm, var = "NicheBreadth")
+emt_algorithm_suitarea = emtrends(suitarea_resp_var, ~Algorithm, var = "NicheBreadth")
+emt_algorithm_elevcentroid = emtrends(elevcentroid_resp_var, ~Algorithm, var = "NicheBreadth")
+
+# How each ResponceVariable changes when NicheBreadth changes, within each PAStrategy
+emt_paStrategy_schoener = emtrends(schoener_resp_var, ~PAStrategy, var = "NicheBreadth")
+emt_paStrategy_meansuit = emtrends(meansuit_resp_var, ~PAStrategy, var = "NicheBreadth")
+emt_paStrategy_suitarea = emtrends(suitarea_resp_var, ~PAStrategy, var = "NicheBreadth")
+emt_paStrategy_elevcentroid = emtrends(elevcentroid_resp_var, ~PAStrategy, var = "NicheBreadth")
+
+# How each ResponceVariable changes when NicheBreadth changes, within each PANumber
+emt_paNumber_schoener = emtrends(schoener_resp_var, ~PANumber, var = "NicheBreadth")
+emt_paNumber_meansuit = emtrends(meansuit_resp_var, ~PANumber, var = "NicheBreadth")
+emt_paNumber_suitarea = emtrends(suitarea_resp_var, ~PANumber, var = "NicheBreadth")
+emt_paNumber_elevcentroid = emtrends(elevcentroid_resp_var, ~PANumber, var = "NicheBreadth")
+
+
+schoener_df = as.data.frame(emt_algorithm_schoener) %>%
   mutate(ResponseVariable = "Schoener's D")
 
-meansuit_df = as.data.frame(emm_meansuit) %>%
+meansuit_df = as.data.frame(emt_algorithm_meansuit) %>%
   mutate(ResponseVariable = "Mean Suitablilty")
 
-suitarea_df = as.data.frame(emm_suitarea) %>%
+suitarea_df = as.data.frame(emt_algorithm_suitarea) %>%
   mutate(ResponseVariable = "Suitable Area")
 
-elevcentroid_df = as.data.frame(emm_elevcentroid) %>%
+elevcentroid_df = as.data.frame(emt_algorithm_elevcentroid) %>%
   mutate(ResponseVariable = "Elevation Centroid")
 
 combined_df = bind_rows(schoener_df, meansuit_df, suitarea_df, elevcentroid_df)
-# combined_df
+combined_df
 
-ggplot(combined_df, aes(x = SpeciesType, y = emmean)) +
-  geom_bar(stat = "identity",color = "black") +
-  geom_errorbar(aes(ymin = lower.CL, ymax = upper.CL), width = 0.5) +
-  facet_wrap(~ResponseVariable, scales = "free") +
-  labs(y = "Response Variable Predictions", x = "Species Type")
+ggplot(combined_df, aes(x = NicheBreadth.trend, y = Algorithm, color = ResponseVariable)) +
+  geom_point(size = 3) +
+  geom_errorbar(aes(xmin = lower.CL, xmax = upper.CL), width = 0.2) +
+  geom_vline(xintercept = 0, linetype = "dashed") +
+  labs(
+    x = "NicheBreadth Effect (Slope)",
+    y = "Algorithm",
+    title = "Effect of NicheBreadth on Response Variables by Algorithm")
+
 

28d070c710fe39a5c7969ea50cbf13138ad35c0b
kristinbillings <kris10d@byu.edu>
2026-04-02 14:37:29 -0600

    Updated figure 3

--- PlotNicheBreadthVSResponceVariables.R
+++ PlotNicheBreadthVSResponceVariables.R
@@ -2,7 +2,7 @@ library(ggplot2)
 library(gridExtra)
 
 
-Make_Graph <- function(y_var, color_var) {
+Make_Graph <- function(y_var, color_var, color_palette) {
   my_plot <- ggplot(biomod_data, aes_string(x = "NicheBreadth", y = y_var,color = color_var)) +
     geom_point(size = 1, alpha = 0.1) +
     geom_smooth(size = 2, method = "lm", se = TRUE, linetype = "solid") +
@@ -12,12 +12,13 @@ Make_Graph <- function(y_var, color_var) {
       title = paste0(y_var, " vs Niche Breadth by ",color_var),
       color = color_var
     ) +
+    scale_color_manual(values = color_palette) +
     theme_minimal(base_size = 14) +
     theme(plot.title = element_text(hjust = 0.5, size = 10),
-          axis.title.x = element_text(size = 9),
-          axis.title.y = element_text(size = 9),
-          legend.title = element_text(size = 8),
-          legend.text = element_text(size = 7),
+          axis.title.x = element_text(size = 10),
+          axis.title.y = element_text(size = 10),
+          legend.title = element_text(size = 7),
+          legend.text = element_text(size = 6),
           legend.position = "right"
     )
   
@@ -37,10 +38,15 @@ biomod_data$Species <- as.factor(biomod_data$Species)
 biomod_data$Bootstrap <- as.factor(biomod_data$Bootstrap)
 
 
+algorithm_palette <- c("#F0E442", "#E69F00", "#56B4E9", "#000000")
+paStrategy_palette <- c("#009E73", "#CC79A7")
+paNumber_palette <- c("#D55E00", "#0072B2", "#999999")
+
+
 # Plot NicheBreadth and Schoener's D 
-schoeners_algorithm_plot <- Make_Graph("SchoenersD", "Algorithm")
-schoeners_paStrategy_plot <- Make_Graph("SchoenersD", "PAStrategy")
-schoeners_paNumber_plot <- Make_Graph("SchoenersD", "PANumber")
+schoeners_algorithm_plot <- Make_Graph("SchoenersD", "Algorithm", algorithm_palette)
+schoeners_paStrategy_plot <- Make_Graph("SchoenersD", "PAStrategy", paStrategy_palette)
+schoeners_paNumber_plot <- Make_Graph("SchoenersD", "PANumber", paNumber_palette)
 
 grid.arrange(schoeners_algorithm_plot, 
              schoeners_paStrategy_plot, 
@@ -48,9 +54,9 @@ grid.arrange(schoeners_algorithm_plot,
              ncol = 3, nrow = 1)
 
 # Plot NicheBreadth and MeanSuitability
-meanSuit_algorithm_plot <- Make_Graph("MeanSuitability", "Algorithm")
-meanSuit_paStrategy_plot <- Make_Graph("MeanSuitability", "PAStrategy")
-meanSuit_paNumber_plot <- Make_Graph("MeanSuitability", "PANumber")
+meanSuit_algorithm_plot <- Make_Graph("MeanSuitability", "Algorithm", algorithm_palette)
+meanSuit_paStrategy_plot <- Make_Graph("MeanSuitability", "PAStrategy", paStrategy_palette)
+meanSuit_paNumber_plot <- Make_Graph("MeanSuitability", "PANumber", paNumber_palette)
 
 grid.arrange(meanSuit_algorithm_plot, 
              meanSuit_paStrategy_plot, 
@@ -58,9 +64,9 @@ grid.arrange(meanSuit_algorithm_plot,
              ncol = 3, nrow = 1)
 
 # Plot NicheBreadth and SuitableArea
-suitArea_algorithm_plot <- Make_Graph("SuitableArea", "Algorithm")
-suitArea_paStrategy_plot <- Make_Graph("SuitableArea", "PAStrategy")
-suitArea_paNumber_plot <- Make_Graph("SuitableArea", "PANumber")
+suitArea_algorithm_plot <- Make_Graph("SuitableArea", "Algorithm", algorithm_palette)
+suitArea_paStrategy_plot <- Make_Graph("SuitableArea", "PAStrategy", paStrategy_palette)
+suitArea_paNumber_plot <- Make_Graph("SuitableArea", "PANumber", paNumber_palette)
 
 grid.arrange(suitArea_algorithm_plot, 
              suitArea_paStrategy_plot, 
@@ -68,9 +74,9 @@ grid.arrange(suitArea_algorithm_plot,
              ncol = 3, nrow = 1)
 
 # Plot NicheBreadth and ElevationCentroid
-elevCentroid_algorithm_plot <- Make_Graph("ElevationCentroid", "Algorithm")
-elevCentroid_paStrategy_plot <- Make_Graph("ElevationCentroid", "PAStrategy")
-elevCentroid_paNumber_plot <- Make_Graph("ElevationCentroid", "PANumber")
+elevCentroid_algorithm_plot <- Make_Graph("ElevationCentroid", "Algorithm", algorithm_palette)
+elevCentroid_paStrategy_plot <- Make_Graph("ElevationCentroid", "PAStrategy", paStrategy_palette)
+elevCentroid_paNumber_plot <- Make_Graph("ElevationCentroid", "PANumber", paNumber_palette)
 
 grid.arrange(elevCentroid_algorithm_plot, 
              elevCentroid_paStrategy_plot, 
@@ -78,9 +84,9 @@ grid.arrange(elevCentroid_algorithm_plot,
              ncol = 3, nrow = 1)
 
 # Plot NicheBreadth and TSS
-tss_algorithm_plot <- Make_Graph("TSS", "Algorithm")
-tss_paStrategy_plot <- Make_Graph("TSS", "PAStrategy")
-tss_paNumber_plot <- Make_Graph("TSS", "PANumber")
+tss_algorithm_plot <- Make_Graph("TSS", "Algorithm", algorithm_palette)
+tss_paStrategy_plot <- Make_Graph("TSS", "PAStrategy", paStrategy_palette)
+tss_paNumber_plot <- Make_Graph("TSS", "PANumber", paNumber_palette)
 
 grid.arrange(tss_algorithm_plot, 
              tss_paStrategy_plot, 

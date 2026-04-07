a3a42fe94db616da95980e3095158b347881212e
kristinbillings <kris10d@byu.edu>
2026-04-07 15:15:10 -0600

    figure 3 code fixed

--- R/analysis/Generate_Fig_3.R
+++ R/analysis/Generate_Fig_3.R
@@ -27,7 +27,7 @@ Make_Graph <- function(y_var, color_var, color_palette) {
 
 
 # Change the file path to where BioMOD output csv is saved on your computer
-biomod_results_file_path <- "./testFiles/SDM_results_for_LMM_3.csv"
+biomod_results_file_path <- "./data/SDM_results_for_LMM_3.csv"
 biomod_data <- read.csv(biomod_results_file_path)
 
 # makes PAStrategy and PANumber categorical variables 
@@ -43,17 +43,22 @@ paStrategy_palette <- c("#009E73", "#CC79A7")
 paNumber_palette <- c("#D55E00", "#0072B2", "#999999")
 
 
+if (!dir.exists("./output/Fig3")) {
+  dir.create("./output/Fig3")
+}
+
 # Plot NicheBreadth and Schoener's D 
 schoeners_algorithm_plot <- Make_Graph("SchoenersD", "Algorithm", algorithm_palette)
 schoeners_paStrategy_plot <- Make_Graph("SchoenersD", "PAStrategy", paStrategy_palette)
 schoeners_paNumber_plot <- Make_Graph("SchoenersD", "PANumber", paNumber_palette)
 
-grid.arrange(schoeners_algorithm_plot, 
+png("./output/Fig3/NicheVsSchoener.png", width = 15, height = 4, units = "in", res = 300)
+
+S_Plot <- grid.arrange(schoeners_algorithm_plot, 
              schoeners_paStrategy_plot, 
              schoeners_paNumber_plot,
              ncol = 3, nrow = 1)
-
-
+dev.off()
 
 # Code to make 4 more graphs
 
@@ -62,38 +67,52 @@ grid.arrange(schoeners_algorithm_plot,
 # meanSuit_paStrategy_plot <- Make_Graph("MeanSuitability", "PAStrategy", paStrategy_palette)
 # meanSuit_paNumber_plot <- Make_Graph("MeanSuitability", "PANumber", paNumber_palette)
 # 
-# grid.arrange(meanSuit_algorithm_plot, 
+# png("./output/Fig3/NicheVsMeanSuitability.png", width = 10, height = 3, units = "in", res = 300)
+#
+# MS_Plot <- grid.arrange(meanSuit_algorithm_plot, 
 #              meanSuit_paStrategy_plot, 
 #              meanSuit_paNumber_plot,
 #              ncol = 3, nrow = 1)
+# dev.off()
 # 
 # # Plot NicheBreadth and SuitableArea
 # suitArea_algorithm_plot <- Make_Graph("SuitableArea", "Algorithm", algorithm_palette)
 # suitArea_paStrategy_plot <- Make_Graph("SuitableArea", "PAStrategy", paStrategy_palette)
 # suitArea_paNumber_plot <- Make_Graph("SuitableArea", "PANumber", paNumber_palette)
 # 
-# grid.arrange(suitArea_algorithm_plot, 
+# png("./output/Fig3/NicheVsSuitableArea.png", width = 10, height = 3, units = "in", res = 300)
+#
+# SA_Plot <- grid.arrange(suitArea_algorithm_plot, 
 #              suitArea_paStrategy_plot, 
 #              suitArea_paNumber_plot,
 #              ncol = 3, nrow = 1)
+# 
+# dev.off()
 # 
 # # Plot NicheBreadth and ElevationCentroid
 # elevCentroid_algorithm_plot <- Make_Graph("ElevationCentroid", "Algorithm", algorithm_palette)
 # elevCentroid_paStrategy_plot <- Make_Graph("ElevationCentroid", "PAStrategy", paStrategy_palette)
 # elevCentroid_paNumber_plot <- Make_Graph("ElevationCentroid", "PANumber", paNumber_palette)
 # 
-# grid.arrange(elevCentroid_algorithm_plot, 
+# png("./output/Fig3/NicheVsElevationCentroid.png", width = 10, height = 3, units = "in", res = 300)
+#
+# EC_Plot <- grid.arrange(elevCentroid_algorithm_plot, 
 #              elevCentroid_paStrategy_plot, 
 #              elevCentroid_paNumber_plot,
 #              ncol = 3, nrow = 1)
+# 
+# dev.off()
 # 
 # # Plot NicheBreadth and TSS
 # tss_algorithm_plot <- Make_Graph("TSS", "Algorithm", algorithm_palette)
 # tss_paStrategy_plot <- Make_Graph("TSS", "PAStrategy", paStrategy_palette)
 # tss_paNumber_plot <- Make_Graph("TSS", "PANumber", paNumber_palette)
 # 
-# grid.arrange(tss_algorithm_plot, 
+# png("./output/Fig3/NicheVsTSS.png", width = 10, height = 3, units = "in", res = 300)
+#
+# TSS_Plot <- grid.arrange(tss_algorithm_plot, 
 #              tss_paStrategy_plot, 
 #              tss_paNumber_plot,
 #              ncol = 3, nrow = 1)
-
+# 
+# dev.off()
